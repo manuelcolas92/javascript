8a6329a56bc9dfdab137ebf82ad8922a8cb86fbd
manuelcolas92 <118778635+manuelcolas92@users.noreply.github.com>
2023-03-14 13:10:20 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,159 +1,292 @@
-alert("Debes ser mayor de 18 años para usar este sistema");
+// ALERT ----------------------------------------
 
-function reservarEntradas() {
+//alert("Debes ser mayor de 18 años para usar este sistema");
 
-    let libres = 200
-    let nombre = prompt("Ingresá tu nombre")
-    let edad = parseInt(prompt("Ingresa tu edad"))
-    let precioEntrada = 1000
+// VALIDACION DE EDAD ---------------------------
 
-    if (edad >= 18) {
-        alert("Bienvenido/a " + nombre)
-        console.log("usuario válido")
+const inputName = document.querySelector('.inputName');
+const inputAge = document.querySelector('.inputAge');
+const inputButton = document.querySelector('.inputButton');
+
+inputButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const textName = inputName.value;
+    const textAge = inputAge.value;
+
+    if (textAge >= 18) {
+        const user = document.querySelector('.user');
+        user.textContent = textName;
+        document.getElementById('pagina').style.display = "initial";
+        document.getElementById('datosPersona').style.display = "none";
+        document.getElementById('navBar').style.display = "flex";
     }
+
     else {
-        do {
-            alert("Lo siento " + nombre + " debés ser mayor de edad")
-            console.log("usuario inválido")
-            edad = parseInt(prompt("Ingresa tu edad"))
-        }
-        while (edad < 18)
+        const mensajeError = document.querySelector('.mensajeError');
+        mensajeError.innerHTML = "Lo siento " + textName + ", pero tenés que ser mayor de edad para poder usar este sistema.<br><br>Buscá ayuda de un adulto y volvelo a intentar.";
     }
+})
 
-    let pelicula0 = {
-        titulo: "Ironman",
-        genero: "Acción",
-    }
+// PELICULAS (OBJETOS Y ARRAY) ------------------
 
-    let pelicula1 = {
-        titulo: "HarryPotter",
-        genero: "Infantil",
-    }
-
-    let pelicula2 = {
-        titulo: "Juego del Miedo",
-        genero: "Suspenso",
-    }
-
-    let pelicula3 = {
-        titulo: "Titanic",
-        genero: "Drama",
-    }
-
-    let pelicula4 = {
-        titulo: "Avatar",
-        genero: "Acción",
-    }
-
-    let pelicula5 = {
-        titulo: "Borat",
-        genero: "Comedia",
-    }
-
-    const peliculas = [pelicula0, pelicula1, pelicula2, pelicula3]
-    console.log("Cantidad de películas en cartelera " + peliculas.length)
-
-    for (let i = 0; i < peliculas.length; i += 1) {
-        console.log("Las películas son: " + peliculas[i]);
-    }
-
-    alert("La cartelera es:\n \n" + pelicula0.titulo + "\n" + pelicula1.titulo + "\n" + pelicula2.titulo + "\n" + pelicula3.titulo)
-
-    alert("Por ser el primer Jueves del mes, se agregron dos películas nuevas y se quitaron las dos mas antiguas")
-
-    peliculas.push(pelicula4)
-    peliculas.push(pelicula5)
-    peliculas.shift()
-    peliculas.shift()
-
-    for (let i = 0; i < peliculas.length; i += 1) {
-        console.log("Las nuevas películas son: " + peliculas[i]);
-    }
-
-    alert("La nueva cartelera es:\n\n" + pelicula2.titulo + "\n" + pelicula3.titulo + "\n" + pelicula4.titulo + "\n" + pelicula5.titulo)
-
-    let pelicula = (prompt("Ingresá el nombre de la película que quieras ver: \n\n" + pelicula2.titulo + "\n" + pelicula3.titulo + "\n" + pelicula4.titulo + "\n" + pelicula5.titulo))
-
-    if ((pelicula == pelicula2.titulo) || (pelicula == pelicula3.titulo) || (pelicula == pelicula4.titulo) || (pelicula == pelicula5.titulo)) {
-        alert("Tu película es " + pelicula)
-    }
-    else {
-        do {
-            alert("La película no está en cartelera")
-            console.log("película no encontrada")
-            pelicula = (prompt("Ingresá el nombre de la película que quieras ver: \n \n" + pelicula2.titulo + "\n" + pelicula3.titulo + "\n" + pelicula4.titulo + "\n" + pelicula5.titulo))
-        }
-        while ((pelicula != pelicula2.titulo) && (pelicula != pelicula3.titulo) && (pelicula != pelicula4.titulo) && (pelicula != pelicula5.titulo))
-    }
-
-    let reservadas = parseInt(prompt("Indicá el número de entradas que quiera reservar, quedan " + libres + " entradas disponibles" + "\n\n" + "Precio de la entrada: $" + precioEntrada + " cada una"))
-    if (reservadas <= libres) {
-        alert("Acabás de reservar " + reservadas + " entradas para ver " + pelicula + " por un valor de $" + (precioEntrada * reservadas) + ", quedan disponibles " + (libres - reservadas))
-        console.log("se reservaron " + reservadas + " entradas para ver " + pelicula + " quedan disponibles " + (libres - reservadas))
-    }
-    else {
-        do {
-            alert("El número de entradas que querés reservar no está disponible")
-            console.log("entradas no disponibles")
-            reservadas = parseInt(prompt("Indicá el número de entradas que quieras reservar, quedan " + libres + " entradas disponibles"))
-        } while (reservadas > libres)
-    }
+function constructorPelicula(sala, titulo, genero, categoria, entradasDisponibles, imagen) {
+    this.sala = sala;
+    this.titulo = titulo;
+    this.categoria = categoria;
+    this.entradasDisponibles = entradasDisponibles;
+    this.genero = genero;
+    this.imagen = imagen;
 }
 
-function reservarComida() {
+const pelicula0 = new constructorPelicula("SALA 1", "Ironman", "ATP", "Acción", 200, "./images/ironman.jpg");
+const pelicula1 = new constructorPelicula("SALA 2", "Harry Potter", "ATP", "Infantil", 200, "./images/harry.jpg");
+const pelicula2 = new constructorPelicula("SALA 3", "El Orfanato", "+16", "Terror", 200, "./images/orfanato.jpg");
+const pelicula3 = new constructorPelicula("SALA 4", "Juego del Miedo", "+16", "Suspenso", 200, "./images/saw.jpg");
+const pelicula4 = new constructorPelicula("ESTRENO", "Relatos Salvajes", "+13", "Suspenso", 200, "./images/relatos.jpg");
+const pelicula5 = new constructorPelicula("ESTRENO", "Joker", "Drama", "+13", 200, "./images/joker.jpg");
+const pelicula6 = new constructorPelicula("ESTRENO", "IT2", "Terror", "+16", 200, "./images/it.jpg");
+const pelicula7 = new constructorPelicula("ESTRENO", "Madagascar", "ATP", "Infantil", 200, "./images/madagascar.jpg");
 
-    const precioCarrito = []
-    let cantidad = 3
+const peliculas = [pelicula0, pelicula1, pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7];
 
-    let comida0 = {
-        comidaNombre: "Gaseosa",
-        comidaPrecio: 300,
-    }
+// CARTELERA ------------------------------------
 
-    let comida1 = {
-        comidaNombre: "Pochoclos",
-        comidaPrecio: 800,
-    }
-
-    let comida2 = {
-        comidaNombre: "Chocolate",
-        comidaPrecio: 500,
-    }
-
-    let comida3 = {
-        comidaNombre: "Ninguno",
-        comidaPrecio: 0,
-    }
-
-    do {
-        let item = prompt("Con tu combo podés ingresar hasta 3 productos de comida, no importa si se repiten:\n\n" + comida0.comidaNombre + " $" + comida0.comidaPrecio + "\n" + comida1.comidaNombre + " $" + comida1.comidaPrecio + "\n" + comida2.comidaNombre + " $" + comida2.comidaPrecio + "\n\n" + "Escribe como opción la palabra - Ninguno - en caso de no querer seguir eligiendo");
-        if (item == comida0.comidaNombre) {
-            precioCarrito.push(comida0.comidaPrecio)
-            alert("Has seleccionado: " + comida0.comidaNombre)
-        }
-        else if (item == comida1.comidaNombre) {
-            precioCarrito.push(comida1.comidaPrecio)
-            alert("Has seleccionado: " + comida1.comidaNombre)
-        }
-        else if (item == comida2.comidaNombre) {
-            precioCarrito.push(comida2.comidaPrecio)
-            alert("Has seleccionado: " + comida2.comidaNombre)
-        }
-        else if (item == comida3.comidaNombre) {
-            precioCarrito.push(comida3.comidaPrecio)
-            alert("Has seleccionado: " + comida3.comidaNombre)
-        }
-        else { alert("El producto no está disponible") }
-
-    } while (precioCarrito.length < cantidad)
-
-    let res = precioCarrito.reduce(
-        (sum, item) => sum + item,
-        0
-    )
-    alert("El precio final de tu combo es de $" + res + ", que disfrutes de tu función!")
-    console.log("combo generado por $" + res)
+for (let i = 0; i < 4; i += 1) {
+    const cartelera = document.querySelector('.cartelera');
+    const tarjeta = document.createElement("div");
+    tarjeta.className = "tarjeta";
+    cartelera.appendChild(tarjeta);
+    const span = document.createElement("span");
+    span.textContent = peliculas[i].sala;
+    tarjeta.appendChild(span);
+    const img = document.createElement("img");
+    img.src = peliculas[i].imagen;
+    tarjeta.appendChild(img);
 }
 
-reservarEntradas()
-reservarComida()
+// PROXIMOS ESTRENOS ----------------------------
+
+for (let i = 4; i < peliculas.length; i += 1) {
+    const estrenos = document.querySelector('.estrenos');
+    const tarjeta = document.createElement("div");
+    tarjeta.className = "tarjeta";
+    estrenos.appendChild(tarjeta);
+    const span = document.createElement("span");
+    span.textContent = peliculas[i].sala;
+    tarjeta.appendChild(span);
+    const img = document.createElement("img");
+    img.src = peliculas[i].imagen;
+    tarjeta.appendChild(img);
+}
+
+// BOLETERIA (OBJETOS Y ARRAY) ------------------
+
+// Creando los objetos y arrays de productos, e insertandolos en el HTML
+// Creando el carrito vacío y funcionalidades del boton Comprar
+
+function constructorProducto(id, nombre, precio, cantidad, img) {
+    this.id = id,
+        this.nombre = nombre;
+    this.precio = precio;
+    this.cantidad = cantidad;
+    this.img = img;
+}
+
+const producto0 = new constructorProducto(01, "ENTRADA SALA 1 - Turbo", 1100, 1, "https://i.pinimg.com/originals/f0/74/b6/f074b6297d05c31c2de42e189a6d2f78.png");
+const producto1 = new constructorProducto(02, "ENTRADA SALA 2 - 3D", 1000, 1, "https://i.pinimg.com/originals/f0/74/b6/f074b6297d05c31c2de42e189a6d2f78.png");
+const producto2 = new constructorProducto(03, "ENTRADA SALA 3", 900, 1, "https://i.pinimg.com/originals/f0/74/b6/f074b6297d05c31c2de42e189a6d2f78.png");
+const producto3 = new constructorProducto(04, "ENTRADA SALA 4", 900, 1, "https://i.pinimg.com/originals/f0/74/b6/f074b6297d05c31c2de42e189a6d2f78.png");
+const producto4 = new constructorProducto(05, "GASEOSA", 500, 1, "https://i.etsystatic.com/34050632/r/il/63a93a/3621947012/il_570xN.3621947012_qbh9.jpg");
+const producto5 = new constructorProducto(06, "POCHOCLOS", 600, 1, "https://www.maccorns.co.uk/wp-content/uploads/2019/04/popcorn-bag-2.png");
+const producto6 = new constructorProducto(07, "COMBO 1", 1000, 1, "https://tuquejasuma.com/media/cache/2c/7a/2c7a0329f8f0e82c079e1107ce074d71.png");
+const producto7 = new constructorProducto(08, "COMBO 2", 1900, 1, "https://tuquejasuma.com/media/cache/2c/7a/2c7a0329f8f0e82c079e1107ce074d71.png");
+
+
+const productos = [producto0, producto1, producto2, producto3, producto4, producto5, producto6, producto7];
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+productos.forEach((producto) => {
+    const pizarraContent = document.getElementById("pizarraContent")
+    let card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML =
+        `
+    <img src="${producto.img}">
+    <p>${producto.nombre}</p>
+    <p>$${producto.precio}</p>
+    `;
+    pizarraContent.append(card);
+
+    let comprar = document.createElement("button");
+    comprar.className = "addButton";
+    comprar.innerText = "Agregar al carrito";
+    card.append(comprar);
+
+    comprar.addEventListener("click", () => {
+
+        const repeat = carrito.some((repeatProducto) => repeatProducto.id === producto.id);
+        if (repeat) {
+            carrito.map((prod) => {
+                if (prod.id === producto.id) {
+                    prod.cantidad++
+                }
+            });
+        } else {
+            carrito.push({
+                id: producto.id,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                cantidad: producto.cantidad,
+                img: producto.img,
+            });
+        }
+        contadorCarrito();
+        saveLocal();
+        pintarCarrito();
+        console.log(carrito);
+    });
+});
+
+// CARRITO --------------------------------------
+
+// Capturando botones Ver y Borrar carrito, Cantidad carrito y el Modal
+
+const verCarrito = document.getElementById("verCarrito");
+const borrarCarrito = document.getElementById("borrarCarrito");
+const modalContainer = document.getElementById("modalContainer");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+// FUNCION PINTAR CARRITO
+
+const pintarCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modalHeader";
+    modalHeader.innerHTML = `
+        <h1 class="modalHeader-titulo">Carrito</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalButton = document.createElement("button");
+    modalButton.innerText = "x";
+    modalButton.className = "modalHeaderButton";
+    modalHeader.append(modalButton);
+
+    const modalEmpty = document.createElement("p");
+    modalEmpty.className = "carritoEmpty";
+    modalContainer.append(modalEmpty);
+
+    modalButton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    })
+
+    carrito.forEach((producto) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "carritoContent";
+        carritoContent.innerHTML = `
+        <img src="${producto.img}">
+        <div class="item-nom">${producto.nombre}</div>               
+        <div class="item-can">Cantidad: ${producto.cantidad}</div>
+        <div class="item-pre">$${producto.precio * producto.cantidad}</div>        
+    `;
+        modalContainer.append(carritoContent);
+
+        let eliminar = document.createElement("span");
+        eliminar.innerHTML = "X";
+        eliminar.className = "deleteProducto";
+        carritoContent.append(eliminar);
+        eliminar.addEventListener("click", eliminarProducto);
+    });
+
+    const total = carrito.reduce((acc, el) => acc + (el.precio * el.cantidad), 0);
+
+    const totalCompra = document.createElement("div");
+    modalContainer.append(totalCompra);
+    totalCompra.className = "totalCompra";
+    totalCompra.innerHTML = `
+        <p class= "totalCarrito">TOTAL</p>
+        <p class= "totalCarritoPrecio">$${total}</p>
+    `;
+    modalContainer.append(totalCompra);
+
+    if (carrito.length === 0) {
+        modalEmpty.innerText = "El carrito está vacío :("
+    }
+    else {
+        modalEmpty.innerText = ""
+    }
+};
+
+verCarrito.addEventListener("click", pintarCarrito);
+
+// FUNCION CONTADOR CARRITO
+
+const contadorCarrito = () => {
+    cantidadCarrito.style.display = "initial";
+    const carritoLength = carrito.length;
+    localStorage.setItem("carritoLength", JSON.stringify(carrito.length))
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+}
+
+// FUNCION VACIAR CARRITO
+
+const vaciarCarrito = () => {
+    for (let i = 0; i < carrito.length; i += 1) {
+        carrito.splice(i);
+        contadorCarrito();
+        saveLocal();
+        pintarCarrito();
+        console.log(carrito);
+    };
+};
+
+borrarCarrito.addEventListener("click", vaciarCarrito);
+
+// FUNCION ELIMINAR PRODUCTO CON LA X POR SU ID
+
+const eliminarProducto = () => {
+    const foundId = carrito.find((element) => element.id);
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== foundId;
+    });
+    contadorCarrito();
+    saveLocal();
+    pintarCarrito();
+}
+
+// LOCALSTORAGE ---------------------------------
+
+// Creando la funcion para guardar en localStorage
+
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+// CONTACTO (SCRIPT DEL FORM) -------------------
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+        btn.value = 'Enviando mensaje...';
+        const serviceID = 'default_service';
+        const templateID = 'template_y0aome1';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Enviar Mensaje';
+                const mensajeEnviado = document.querySelector('.mensajeEnviado');
+                const mensaje = document.createElement("p");
+                mensaje.textContent = "Su mensaje ha sido enviado con éxito!";
+                mensajeEnviado.appendChild(mensaje);
+            }, (err) => {
+                btn.value = 'Enviar Mensaje';
+                alert(JSON.stringify(err));
+            });
+    });
+
+    contadorCarrito();
